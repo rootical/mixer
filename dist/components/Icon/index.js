931307e4cf25896bdcec7b1d6dbd20712fd142ab
rootical <rootical.web@gmail.com>
2020-05-25 16:37:52 +0200

    Remove dist and demo

--- dist/components/Icon/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import classnames from 'classnames';
-import style from './style.css';
-const Icon = ({ type }) => (React.createElement("div", { className: classnames(style.icon, style[`icon_type_${type}`]) }));
-export default Icon;
-//# sourceMappingURL=index.js.map
