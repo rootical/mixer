a410551fd778f4ae42d47fcfae8557091a4cf202
Vadzim Jakušaŭ <rootical.web@gmail.com>
2020-05-26 17:36:46 +0200

    Updates

---
+++ precache-manifest.aca654cbdc3247ec02b37605fc9fd2c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e649cd8d5e147e7a5b41328e97f27da",
+    "url": "/mixer/index.html"
+  },
+  {
+    "revision": "32ecfdf5deeb168a8cb7",
+    "url": "/mixer/static/css/main.71dcf0ac.chunk.css"
+  },
+  {
+    "revision": "92f242efe0fe21bd9e37",
+    "url": "/mixer/static/js/2.8d65340e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mixer/static/js/2.8d65340e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32ecfdf5deeb168a8cb7",
+    "url": "/mixer/static/js/main.b06ed9dd.chunk.js"
+  },
+  {
+    "revision": "3fc6ff4c81cd6a523bee927348ab17ad",
+    "url": "/mixer/static/js/main.b06ed9dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aae796ef153ae936ab8d",
+    "url": "/mixer/static/js/runtime-main.8a23f0c5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-mixdesk/precache-manifest.fe58a23ceba47ec04b0b4a0ff8087873.js"
+  "/mixer/precache-manifest.aca654cbdc3247ec02b37605fc9fd2c1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-mixdesk/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mixer/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
