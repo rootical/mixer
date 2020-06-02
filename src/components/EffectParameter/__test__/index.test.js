3a7a26ffd51d12af7a0b1e09cca7803d6690a6f0
Vadzim Jakušaŭ <rootical.web@gmail.com>
2020-06-02 18:22:17 +0200

    Attempt to fix tests

--- src/components/EffectParameter/__test__/index.test.js
+++ src/components/EffectParameter/__test__/index.test.js
@@ -12,7 +12,7 @@ const propsMock = {
 };
 
 describe('<EffectParameter />', () => {
-	it('should renders without any errors', () => {
+	it('should render without any errors', () => {
 		const wrapper = shallow(<EffectParameter {...propsMock} />);
 
 		expect(wrapper).toMatchSnapshot();
