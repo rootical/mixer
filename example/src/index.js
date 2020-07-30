70bba59befd8fa3ec0d2aa4cbe772eaddebb53d2
Vadzim Jakušaŭ <rootical.web@gmail.com>
2020-07-30 18:49:20 +0200

    😞

--- example/src/index.js
+++ example/src/index.js
@@ -1,30 +1,30 @@
-import 'react-mixdesk/dist/index.css';
+import 'react-mixdesk/dist/index.css'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {Mixdesk} from 'react-mixdesk';
+import { Mixdesk } from 'react-mixdesk'
 
 import './index.css'
 
-
 const defaultTracks = [
-    {
-        title : 'Click',
-        url : 'https://s3.amazonaws.com/worship-online/song_audio_mixer_tracks/audios/000/000/009/original/CLICK.m4a?1594846181'
-    },
-    {
-        title : 'Guide',
-        url : 'https://s3.amazonaws.com/worship-online/song_audio_mixer_tracks/audios/000/000/021/original/GUIDE.m4a?1594846181'
-    },
-    {
-        title : 'Drums',
-        url : 'https://s3.amazonaws.com/worship-online/song_audio_mixer_tracks/audios/000/000/033/original/DRUMS.m4a?1594846181'
-    },
-    {
-        title : 'Melodies',
-        url : 'assets/vox.mp3'
-    }
-];
+  {
+    title: 'Click',
+    url:
+      'https://s3.amazonaws.com/worship-online/song_audio_mixer_tracks/audios/000/000/009/original/CLICK.m4a?1594846181'
+  },
+  {
+    title: 'Guide',
+    url:
+      'https://s3.amazonaws.com/worship-online/song_audio_mixer_tracks/audios/000/000/021/original/GUIDE.m4a?1594846181'
+  },
+  {
+    title: 'Drums',
+    url:
+      'https://s3.amazonaws.com/worship-online/song_audio_mixer_tracks/audios/000/000/033/original/DRUMS.m4a?1594846181'
+  }
+]
 
-
-ReactDOM.render(<Mixdesk tracks={defaultTracks} />, document.getElementById('root'));
+ReactDOM.render(
+  <Mixdesk tracks={defaultTracks} />,
+  document.getElementById('root')
+)
