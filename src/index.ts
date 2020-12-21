670dd3e5db790670627efac6cf4147822c897a4c
Vadzim Jakušaŭ <rootical.web@gmail.com>
2020-12-21 11:16:42 +0100

    chore: bump version

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export * from './components';
 export * from './constants';
 export * from './containers';
 export * from './helpers';
+export * from './hooks';
 export * from './models';
 export * from './store';
